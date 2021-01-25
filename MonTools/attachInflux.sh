e64e8159c49ec496a0444bf4df4c13cf7203d27f
LIG Docker Deployment <rhamilton@lowell.edu>
2021-01-25 20:52:42 +0000

    Lots of updates to LDT monitoring base stuff
    Includes Loki and Logstash

--- MonTools/attachInflux.sh
+++ MonTools/attachInflux.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# DB client
 docker-compose up -d influxdb; docker exec -it influxdb influx
+
+# If you need a shell
+#docker-compose up -d influxdb; docker exec -it influxdb /bin/bash
+
